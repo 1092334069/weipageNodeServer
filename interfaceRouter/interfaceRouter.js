3019cda60518803de07986625b28b15569bac130
linguibin <1092334069@qq.com>
2019-06-07 16:30:12 +0800

    新增接口

--- interfaceRouter/interfaceRouter.js
+++ interfaceRouter/interfaceRouter.js
@@ -3,7 +3,7 @@ const InterfaceInfo = require('../interface/interfaceInfo')
 const interfaceInfo = new InterfaceInfo()
 
 const router = [{
-	pathname: '/api/user/detail',
+	pathname: '/api/interface/insert',
 	action: interfaceInfo.insert,
 	isLogin: true
 }]
