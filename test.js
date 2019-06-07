556a10022803f60a31cfa43c18ec6ca78cf76356
linguibin <1092334069@qq.com>
2019-06-07 16:18:17 +0800

    测试新增接口

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const InterfaceInfo = require('./interface/interfaceInfo')
 const interfaceInfo = new InterfaceInfo()
 
 interfaceInfo.insert({
+	userId: 1,
 	name: '测试第一个接口',
 	url: 'http://zhinengluodiye.com/test/first',
 	callbackParam: '[{"key":"code","name":"状态码","type":"number"},{"key":"msg","name":"消息","type":"string"}]'
