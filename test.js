22ec2bc25fa9146c5aee5c4d517081b298770dd2
linguibin <1092334069@qq.com>
2019-06-09 21:43:30 +0800

    测试图片资源列表

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 const ImageInfo = require('./interface/imageInfo')
 const imageInfo = new ImageInfo()
 
-imageInfo.detail({
+// imageInfo.detail({
+// 	userId: 1,
+// 	imageId: 2
+// }, (res) => {
+// 	console.log(res)
+// })
+getPageList.getPageList({
 	userId: 1,
-	imageId: 2
+	page: 1,
+	size: 2
 }, (res) => {
 	console.log(res)
 })
