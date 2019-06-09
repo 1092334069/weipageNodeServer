f9bc9dbc8889996be8d45a52f4c00c79212063cb
linguibin <1092334069@qq.com>
2019-06-09 14:20:49 +0800

    测试修改微页面

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 const WeipageInfo = require('./interface/weipageInfo')
 const weipageInfo = new WeipageInfo()
 
-weipageInfo.insert({
+weipageInfo.update({
 	userId: 1,
+	weipageId: 2,
 	name: '第二个微页面',
-	describes: '描述',
+	describes: '描述2',
 	cover: 'http://img.aisqueezepage.com.com/2.png',
 	pageName: 'activity',
 	data: '{"pluginList":[],"requestList":[]}'
