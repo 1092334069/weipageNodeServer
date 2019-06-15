4c1496e0e2aab3236bb1f7a8dfda50a5e574b640
linguibin <1092334069@qq.com>
2019-06-15 15:06:21 +0800

    测试加密解密

--- test.js
+++
@@ -1,9 +0,0 @@
-const aesUtil = require('./libs/aesUtil')
-
-const uid = 1
-
-const res = aesUtil.encrypt(uid.toString())
-console.log(res)
-
-const r = aesUtil.decrypt('0e38bc264f47dcec9edd6b21bb95d8f6')
-console.log(parseInt(r))
